71588f4c11de2411a6422c0c2502db35b7dae38c
Serch12 <sergio@amfpro.mx>
2024-06-17 11:16:06 -0600

    cambios en contacto

--- resources/js/components/Contacto.vue
+++ resources/js/components/Contacto.vue
@@ -2,9 +2,10 @@
     <div>
         <div id="main">
             <div>
-                
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.2284477760422!2d-99.22412718511622!3d19.31589078695073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdff9746b23525%3A0x3971e733c98b593d!2sRehab%20Evolution!5e0!3m2!1ses!2smx!4v1662394136964!5m2!1ses!2smx" 
-                style="border:0; width:1230px;padding:5px;margin:15px;" allowfullscreen=""></iframe>
+                <center>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.2284477760422!2d-99.22412718511622!3d19.31589078695073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdff9746b23525%3A0x3971e733c98b593d!2sRehab%20Evolution!5e0!3m2!1ses!2smx!4v1662394136964!5m2!1ses!2smx" 
+                    style="border:0; width:1230px;padding:5px;margin:15px;" allowfullscreen=""></iframe>
+                </center>
             </div>
             <p style="color:#00ABE1;font-size:35px;font-weight:800;text-align:center;margin:auto;padding:5px;">CONTACTO</p>
             <div class="container pb-5">
